c75d59ca1fd9bd55683d1eb86f38f9f10aae875c
Gustavo <gustavoeduardoneiragonzalez@gmail.com>
2021-09-04 03:37:43 -0400

    avanze de pana zi

--- src/views/ComienzaReciclar.vue
+++ src/views/ComienzaReciclar.vue
@@ -1,5 +1,56 @@
 <template>
     <div>
         <h3>asd</h3>
+        <h2>asdasdasdsad</h2>
     </div>
+
+    <section class="contenedor recicla">
+            <div class="contenedor Servicios">
+                <h2 class="titulo">Este gordo hijo de puta recicla como una bestia quieres saber como click aqui</h2>
+                <div class="servicio contenedor">
+                    <div class="contenedor-reciclaCard">
+                        <img class="imagenRecycla" src="../assets/recycle.jpg" alt="asd">
+                        <h3>Name</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, molestiae?</p>
+                    </div>
+                </div>
+            </div>
+    </section>
+    <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150"
+    preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.28,50.81 C-82.67,230.42 368.79,-8.38 502.54,65.63 L517.21,164.30 L-7.62,150.48 Z"
+    style="stroke: none; fill: #08f;"></path></svg></div>
 </template>
+
+<style scope>
+
+    .contenedor{
+            padding: 15px;
+            height: 500px;
+            width: 100%;
+            max-height: 500px;
+            overflow: hidden;
+            background-color: #1c7c54 ;
+        }
+
+    .titulo{
+        text-decoration:underline;
+        }
+
+    .contenedor-reciclaCard{
+            width:50%;
+            height: 80%;
+            background-color: black;
+            display: flex;
+            justify-content: flex-start;
+        }
+
+    .imagenRecycla{
+        width: 40%;
+        max-width: 40%;
+        height: 100%;
+        max-height: 50%;
+        display:flex;
+        justify-content: flex-start;
+        }
+
+</style>
