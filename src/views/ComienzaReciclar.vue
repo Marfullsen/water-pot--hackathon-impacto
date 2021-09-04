231e88f894f41140d8235c6c8bab67c3adb66748
Gustavo <gustavoeduardoneiragonzalez@gmail.com>
2021-09-04 06:02:19 -0400

    Avanze primera ola, e incorporación mapa sin diseño

--- src/views/ComienzaReciclar.vue
+++ src/views/ComienzaReciclar.vue
@@ -6,19 +6,28 @@
 
     <section class="contenedor recicla">
             <div class="contenedor Servicios">
-                <h2 class="titulo">Este gordo hijo de puta recicla como una bestia quieres saber como click aqui</h2>
+                <h2 class="titulo">Recicla ¡YA!</h2>
                 <div class="servicio contenedor">
                     <div class="contenedor-reciclaCard">
+                        <h3>Es tu deber</h3>
                         <img class="imagenRecycla" src="../assets/recycle.jpg" alt="asd">
-                        <h3>Name</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, molestiae?</p>
+                        <p class="reciclaP">Gracias a ti se podrá tener un futuro mejor</p>
                     </div>
                 </div>
             </div>
     </section>
-    <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150"
-    preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.28,50.81 C-82.67,230.42 368.79,-8.38 502.54,65.63 L517.21,164.30 L-7.62,150.48 Z"
-    style="stroke: none; fill: #08f;"></path></svg></div>
+
+<div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+ style="height: 100%; width: 100%;"><path d="M0.00,49.98 C149.99,150.00 271.49,-49.98 500.00,49.98 L500.00,0.00 L0.00,0.00 Z"
+ style="stroke: none; fill: #1c7c54 ;"></path></svg></div>
+
+ <section class="contenedorMapa">
+     <div class="mapaDiv">
+
+     </div>
+     <iframe class="mapa" src="https://www.google.com/maps/d/embed?mid=1Y_rRDHYD9VBEVNz2LJIldKU-j4U" width="640" height="480"></iframe>
+     <img src="../assets/Tree-T-PEE2-2.jpg" alt="">
+ </section>
 </template>
 
 <style scope>
@@ -31,26 +40,35 @@
             overflow: hidden;
             background-color: #1c7c54 ;
         }
-
+    .wave{
+        position: absolute;
+        width: 100%;
+        }
     .titulo{
         text-decoration:underline;
+        font-family: 'Fredoka One', cursive;
         }
 
     .contenedor-reciclaCard{
-            width:50%;
+            width:100%;
             height: 80%;
-            background-color: black;
+            padding-top: 20px;
             display: flex;
-            justify-content: flex-start;
+            flex-direction: column;
+            align-items: center;
+            justify-content: space-between;
         }
 
     .imagenRecycla{
-        width: 40%;
-        max-width: 40%;
-        height: 100%;
-        max-height: 50%;
-        display:flex;
-        justify-content: flex-start;
+        width: 15%;
+        max-width: 50%;
+        height: 40%;
+        max-height: 100%;
+        border-radius: 25%;
+        }
+
+    .mapa{
+        border-radius: 60%;
         }
 
 </style>
