0f78b1259d875ef3ac903dfffc7a9b12f5789d8a
Alfajor <alv.moralesm@duocuc.cl>
2021-09-04 01:42:43 -0400

    Mas IMGs

--- src/views/PaginaPrincipal.vue
+++ src/views/PaginaPrincipal.vue
@@ -3,30 +3,45 @@
     <div class="container.fluid">
       <div class="row">
         <div class="col-4">
-          <img class="img-1 img-fluid" src="../assets/recycle.jpg" alt="">
+          <img class="img-index img-fluid" src="../assets/recycle.jpg" alt="">
         </div>
         <div class="col-8">
+          <h2>¿Qué Es?</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas distinctio adipisci sapiente. Quisquam odio veniam rem! Eveniet sequi, quia, totam nemo placeat error eum rerum quae ducimus quod quas adipisci.
+          Excepturi corrupti veritatis odit maiores, temporibus accusantium eos quibusdam consequatur quam eveniet architecto. Provident officiis natus earum culpa eveniet, fugiat, a sed consequatur accusantium iusto id quisquam dignissimos temporibus molestiae.
+          Eius obcaecati eligendi quo et optio deleniti nisi nemo incidunt vitae accusamus cum, ad quas blanditiis inventore eos mollitia esse tenetur, voluptas quod porro eveniet culpa. Ducimus natus nostrum cupiditate?</p>
         </div>
       </div>
 
       <div class="row">
         <div class="col-8">
+          <h2>¿Para qué?</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos tenetur unde reprehenderit minus nam fugiat saepe quia, quis eum quo provident facilis ipsam, molestias nihil similique delectus corrupti cumque nemo.
+          Illum amet, repellat accusantium voluptates optio ut totam natus ullam pariatur quia delectus hic ea expedita cupiditate iste. Maiores fugiat impedit accusantium minima aut eaque alias fuga dolore odit harum!</p>
         </div>
         <div class="col-4">
-          <img class="img-1 img-fluid" src="../assets/recycle.jpg" alt="">
+          <img class="img-index img-fluid" src="../assets/recycle.jpg" alt="">
         </div>
       </div>
 
       <div class="row">
         <div class="col-4">
-          <img class="img-1 img-fluid" src="../assets/recycle.jpg" alt="">
+          <img class="img-index img-fluid" src="../assets/recycle.jpg" alt="">
         </div>
         <div class="col-8">
+          <h2>¿Por qué?</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatum, quae, minus repudiandae ut sit, itaque magnam qui laboriosam hic minima quibusdam! Aperiam, sit quasi! Odit sunt quae totam a.
+          Autem necessitatibus aut accusantium, distinctio adipisci non dolorem unde nesciunt expedita fugit corrupti rem fuga nemo perferendis aliquid possimus eligendi, in saepe facilis corporis placeat illo quibusdam odit deleniti. Impedit?</p>
         </div>
       </div>
 
       <div class="row">
         <div class="col-8">
+          <h2>Tipo de Water Pot</h2>
+          <br>
+          <p>- Blanco: Water Pot orientado a árboles de climas fríos. <br><br>
+            - Negro: Water Pot orientado a árboles de climas cálidos.
+          </p>
         </div>
         <div class="col-4">
           <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
@@ -36,17 +51,13 @@
             </div>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src="../assets/apple-trees.jpg" class="d-block w-100" alt="...">
+                <img src="../assets/Black-Tree-T-PEE.jpg" class="t-wp d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                  <h5>First slide label</h5>
-                  <p>Some representative placeholder content for the first slide.</p>
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="../assets/apple-trees.jpg" class="d-block w-100" alt="...">
+                <img src="../assets/White-Tree-T-PEE.jpg" class="t-wp d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
-                  <h5>Second slide label</h5>
-                  <p>Some representative placeholder content for the second slide.</p>
                 </div>
               </div>
             </div>
@@ -68,8 +79,7 @@
             <h2>Beneficios</h2>
             <br>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam dolore reiciendis adipisci maxime voluptatibus unde. Sed repudiandae reprehenderit tempora quis quam. Hic est velit quibusdam voluptates minus quia unde nulla.
-            Quae nemo incidunt, tempora quo esse ex odio quibusdam facere, in ipsam nulla porro tempore aut itaque corrupti corporis placeat officiis! In voluptatem iure debitis distinctio voluptatum cupiditate vitae excepturi!
-            Ullam incidunt, dolorem corporis libero laudantium repellendus similique culpa, autem aliquam ut nam sed ipsa quis accusantium nostrum atque nihil iure consequatur tempora expedita quod eum? Aut id eius voluptate?</p>
+            Quae nemo incidunt, tempora quo esse ex odio quibusdam facere, in ipsam nulla porro tempore aut itaque corrupti corporis placeat officiis! In voluptatem iure debitis distinctio voluptatum cupiditate vitae excepturi!</p>
           </div>
         </div>
       </div>
@@ -90,17 +100,30 @@ export default {
 <style scoped>
   main{
     background-color: #def4c6;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+  h2{
+    padding-top: 6vh;
+    font-size: 4vh;
+  }
+  p{
+    padding-top: 4vh;
+    font-size: 2vh;
   }
   .col-8{
     background-color: #1c7c54 ;
+    color: #def4c6;
+    padding: 3vh 3vh;
   }
   .col-4{
     padding:0;
   }
-  .col-8:nth-of-type(2){
+  .col-8:nth-of-type(2), .col-12{
     background-color: #def4c6;
+    color: #1c7c54;
   }
-  .col-12{
-    background-color: #def4c6;
+  .t-wp{
+    height: 428px;
+    width: 642px;
   }
 </style>
