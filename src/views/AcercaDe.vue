818c362527af8350693c022cdf978c24917cdb03
Negro <fabian_1244@hotmail.com>
2021-09-04 00:42:03 -0400

    Feature: imagen con texto

--- src/views/AcercaDe.vue
+++ src/views/AcercaDe.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <h1>¿Quienes somos?</h1>
-  </div>
+    <div class="acerca-de">
+        <h1>Nuestro equipo</h1>
+    </div>
 </template>
+
+<style>
+h1{
+  color: white;
+  background: transparent;
+  margin: 0;
+}
+img{
+  width: 100%;
+}
+
+.acerca-de{
+  background: url('../assets/water-drop.jpg');
+  background-repeat: no-repeat;
+  background-size:100%;
+  min-height: 100vh;
+  }
+</style>
