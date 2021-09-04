0fc3f6d45a4b61667e0c6aafc557110e266b56f0
Alfajor <alv.moralesm@duocuc.cl>
2021-09-04 03:36:39 -0400

    f-index

--- src/views/PaginaPrincipal.vue
+++ src/views/PaginaPrincipal.vue
@@ -2,58 +2,50 @@
   <main>
     <div class="container.fluid">
       <div class="row">
-        <div class="col-4">
-          <img class="img-index img-fluid" src="../assets/Tree-T-PEE2-2.jpg" alt="">
-        </div>
-        <div class="col-8">
+        <div class="vc col-lg-8 col-md-12 col-sm-12">
           <h2>¿Qué Es?</h2>
           <p>El Water Pot es un contenedor de agua y fertilizantes, construido a base de plástico reciclado. El dispositivo se ubica en la base del árbol en cuestión.</p>
         </div>
+        <div class="col-lg-4 col-md-12 col-sm-12">
+          <img class="img-index img-fluid" src="../assets/Tree-T-PEE2-2.jpg" alt="">
+        </div>
       </div>
 
       <div class="row">
-        <div class="col-8">
+        <div class="col-lg-4 col-md-12 col-sm-12">
+          <img class="img-index img-fluid" src="../assets/irrigation.jpg" alt="">
+        </div>
+        <div class="vo col-lg-8 col-md-12 col-sm-12">
           <h2>¿Para qué?</h2>
           <p>El Water Pot cumple muchas funciones, dentro de ellas. Optimizar el método de riego, esto lo hace gracias a una forma más ideal de riego, a través de goteo. Otra de sus funciones es protegerlos de temperaturas frías, ya qué también cumple el rol de un mini-invernadero.</p>
         </div>
-        <div class="col-4">
-          <img class="img-index img-fluid" src="../assets/irrigation.jpg" alt="">
-        </div>
       </div>
 
       <div class="row">
-        <div class="col-4">
+        <div class="vc col-lg-8 col-md-12 col-sm-12">
+          <h2>¿Por qué?</h2>
+          <p>Porque en métodos de riego convencionales se pierden cantidades sustanciales de recursos como agua, fertilizante y combustible para el bombeo de agua. Esto quiere decir que el motivo principal detrás de la idea del Water Pot es la escacez de agua a nivel global, específicamente en Chile y el desperdicio de recursos en general.</p>
+        </div>
+        <div class="col-lg-4 col-md-12 col-sm-12">
           <img class="img-index img-fluid" src="../assets/climate-1.jpg" alt="">
         </div>
-        <div class="col-8">
-          <h2>¿Por qué?</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque voluptatum, quae, minus repudiandae ut sit, itaque magnam qui laboriosam hic minima quibusdam! Aperiam, sit quasi! Odit sunt quae totam a.
-          Autem necessitatibus aut accusantium, distinctio adipisci non dolorem unde nesciunt expedita fugit corrupti rem fuga nemo perferendis aliquid possimus eligendi, in saepe facilis corporis placeat illo quibusdam odit deleniti. Impedit?</p>
-        </div>
       </div>
 
       <div class="row">
-        <div class="col-8">
-          <h2>Tipo de Water Pot</h2>
-          <br>
-          <p>- Blanco: Water Pot orientado a árboles de climas fríos. <br><br>
-            - Negro: Water Pot orientado a árboles de climas cálidos.
-          </p>
-        </div>
-        <div class="col-4">
-          <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="col-lg-4 col-md-12 col-sm-12">
+          <div id="carouselExampleCaptions" class="carousel slide carousel" data-bs-ride="carousel">
             <div class="carousel-indicators">
               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
               <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
             </div>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src="../assets/Black-Tree-T-PEE.jpg" class="t-wp d-block w-100" alt="...">
+                <img src="../assets/wpb.png" class="t-wp d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                 </div>
               </div>
               <div class="carousel-item">
-                <img src="../assets/White-Tree-T-PEE.jpg" class="t-wp d-block w-100" alt="...">
+                <img src="../assets/wpw.png" class="t-wp d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                 </div>
               </div>
@@ -68,15 +60,26 @@
             </button>
             </div>
         </div>
+        <div class="vo col-lg-8 col-md-12 col-sm-12">
+          <h2>Tipo de Water Pot</h2>
+          <br>
+          <p>• Blanco: Water Pot orientado a árboles de climas fríos. <br><br>
+            • Negro: Water Pot orientado a árboles de climas cálidos.
+          </p>
+        </div>
       </div>
       <div class="container">
         <div class="row">
-          <div class="col-12">
+          <div class="vc col-12">
             <br><br>
             <h2>Beneficios</h2>
             <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam dolore reiciendis adipisci maxime voluptatibus unde. Sed repudiandae reprehenderit tempora quis quam. Hic est velit quibusdam voluptates minus quia unde nulla.
-            Quae nemo incidunt, tempora quo esse ex odio quibusdam facere, in ipsam nulla porro tempore aut itaque corrupti corporis placeat officiis! In voluptatem iure debitis distinctio voluptatum cupiditate vitae excepturi!</p>
+            <p>• Promueve el crecimiento de raíces. <br>
+              • Impulsa el crecimiento del cultivo en un 30% <br>
+              • Reducción en el uso de recursos, como: agua, fertilizantes y energía (electricidad/combustible). La disminución en el uso de recursos puede llegar incluso a cifras del 90% <br>
+              • Aumenta la temperatura interna del Water Pot gracias a su efecto invernadero, lo que protege los árboles contra bajas temperaturas. <br>
+              • Protege contra el viento, llevando al desarrollo de un tronco sólido y estable.
+            </p>
           </div>
         </div>
       </div>
@@ -100,27 +103,35 @@ export default {
     font-family: Verdana, Geneva, Tahoma, sans-serif;
   }
   h2{
-    padding-top: 4vh;
+    font-family: 'Fredoka One', cursive;
+    /* font-family: 'Righteous', cursive; */
+    /* font-family: 'Staatliches', cursive; */
+    /* font-family: 'Russo One', sans-serif; */
+    padding-top: 6vh;
     font-size: 4vh;
+    font-weight: 700;
+    letter-spacing: 1.3vh;
+    text-transform: uppercase;
   }
   p{
-    font-size: 2vh;
-    padding: 5vh 20vh;
+    font-size: 3vh;
+    padding: 5vh 7vh;
+    font-family: 'Hind Vadodara', sans-serif;
+    letter-spacing: 0.4vh;
   }
-  .col-8{
-    background-color: #1c7c54 ;
+  .vo{
+    background-color: #1c7c54;
     color: #def4c6;
-    padding: 3vh 3vh;
   }
-  .col-4{
-    padding:0;
-  }
-  .col-8:nth-of-type(2), .col-12{
+  .vc{
     background-color: #def4c6;
     color: #1c7c54;
   }
-  .col-12{
-    padding-bottom: 5vh;
+  .col-lg-4{
+    padding:0;
+  }
+  .img-index{
+    padding:0;
   }
   .t-wp{
     height: 428px;
