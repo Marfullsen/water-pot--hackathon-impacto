4c11f352eae5dc6ed9a2ab4adc431c21799987f1
Alfajor <alv.moralesm@duocuc.cl>
2021-09-04 20:15:09 -0400

    imgsimgs

---
+++ src/views/AportaYa.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <div class="containerfluid">
+      <div class="row">
+        <div class="drought-img col-9"></div>
+      </div>
+      <div class="row">
+        <div class="recycle-img col-12"></div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'AportaYa'
+}
+</script>
+<style scoped>
+  .drought-img{
+    background: url('../assets/climate-7.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 92vh;
+  }
+  .recycle-img{
+    background: url('../assets/recycle.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: 92vh;
+  }
+</style>
